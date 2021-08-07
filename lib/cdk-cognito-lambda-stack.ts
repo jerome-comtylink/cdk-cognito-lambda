9db60914d1436e4f184f38d9267be5807cffba8c
Jerome Ky <jerome@comtylink.com>
2021-08-07 19:33:10 +1000

    first commit

--- lib/cdk-cognito-lambda-stack.ts
+++ lib/cdk-cognito-lambda-stack.ts
@@ -1,9 +1,39 @@
+import { Function, Code, Runtime } from '@aws-cdk/aws-lambda';
+import { AccountRecovery, CfnUserPool, UserPool, UserPoolTriggers } from '@aws-cdk/aws-cognito';
 import * as cdk from '@aws-cdk/core';
 
 export class CdkCognitoLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new Function(this, 'MyFunction', {
+      runtime: Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: Code.fromInline('exports.handler = function(event, ctx, cb) { return cb(null, "hello world"); }')
+    });
+
+    new UserPool(this, `userpool-1`, {
+      userPoolName: 'userpool',
+      accountRecovery: AccountRecovery.EMAIL_ONLY,
+      autoVerify: {
+        email: true,
+      },
+      lambdaTriggers: {
+        preTokenGeneration: fn,
+      },
+    });
+    
+
+    const userPool = new UserPool(this, `userpool-2`, {
+      userPoolName: 'userpool',
+      accountRecovery: AccountRecovery.EMAIL_ONLY,
+      autoVerify: {
+        email: true,
+      },
+      lambdaTriggers: {
+        preTokenGeneration: fn,
+      },
+    });
+
   }
 }
